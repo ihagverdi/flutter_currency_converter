2c321e0bdab415eb8bf9ca8fb5d1d3a38d76f894
MohammedSamhouri20 <66756821+MohammedSamhouri20@users.noreply.github.com>
2022-12-14 10:51:23 +0100

    landscape

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -78,6 +78,8 @@ class _MyHomeState extends State<MyHome> {
 
   @override
   Widget build(BuildContext context) {
+    final Context = MediaQuery.of(context).orientation;
+
     return Scaffold(
       backgroundColor: Colors.green[50],
       appBar: AppBar(
@@ -105,11 +107,13 @@ class _MyHomeState extends State<MyHome> {
       ),
       body: _pageLoading == true
           ? Center(child: CircularProgressIndicator())
-          : buildHome(),
+          : (Context == Orientation.portrait
+              ? buildPortrait()
+              : buildLandscape()),
     );
   }
 
-  Widget buildHome() {
+  Widget buildPortrait() {
     return Container(
       margin: EdgeInsets.only(top: 10.0),
       child: ListView(
@@ -218,7 +222,133 @@ class _MyHomeState extends State<MyHome> {
       ),
     );
   }
+
+  Widget buildLandscape() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      child: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10.0),
+            //color: Colors.red,
+            child: TextFormField(
+              controller: amountController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              style: TextStyle(fontSize: 20.0),
+              decoration: InputDecoration(
+                labelText: 'Amount',
+                border: OutlineInputBorder(),
+              ),
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.allow(RegExp(r'[0-9]+[.]?[0-9]*')),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  //color: Colors.red,
+                  padding: EdgeInsets.all(10.0),
+                  child: DropdownButtonFormField(
+                    isExpanded: true,
+                    value: currFrom,
+                    items: myItems(),
+                    onChanged: (val) {
+                      setState(() {
+                        currFrom = val as String;
+                      });
+                    },
+                    icon: Icon(Icons.arrow_drop_down_circle,
+                        color: Colors.green[400]),
+                    dropdownColor: Colors.green[50],
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(fontSize: 20.0),
+                      border: OutlineInputBorder(),
+                      labelText: 'From',
+                      prefixIcon: Container(
+                        padding: EdgeInsets.all(8),
+                        width: 50,
+                        //color: Colors.red,
+                        child: Image.asset(
+                            'icons/currency/${currFrom.toLowerCase()}.png',
+                            package: 'currency_icons'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  color: Colors.green[400],
+                  iconSize: 40.0,
+                  onPressed: () {
+                    setState(() {
+                      var temp = currFrom;
+                      currFrom = currTo;
+                      currTo = temp;
+                    });
+                  },
+                  icon: Icon(Icons.swap_horizontal_circle),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  //color: Colors.red,
+                  padding: EdgeInsets.all(10.0),
+                  child: DropdownButtonFormField(
+                    isExpanded: true,
+                    value: currTo,
+                    items: myItems(),
+                    onChanged: (val) {
+                      setState(() {
+                        currTo = val as String;
+                      });
+                    },
+                    icon: Icon(Icons.arrow_drop_down_circle,
+                        color: Colors.green[400]),
+                    dropdownColor: Colors.green[50],
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(fontSize: 20.0),
+                      border: OutlineInputBorder(),
+                      labelText: 'To',
+                      prefixIcon: Container(
+                        padding: EdgeInsets.all(8),
+                        width: 50,
+                        //color: Colors.red,
+                        child: Image.asset(
+                            'icons/currency/${currTo.toLowerCase()}.png',
+                            package: 'currency_icons'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            myCurrency
+                    .convertCurrency(amountController.text, currFrom, currTo)
+                    .toString() +
+                " " +
+                currTo,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 30.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
+ 
 
 /*
   Widget buildLandscape() {
